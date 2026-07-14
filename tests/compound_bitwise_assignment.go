20566810c2631cd66a7b09c92a7e387434206409
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-07-15 07:45:15 +1000

    backend: preserve operand order for Wasm bit clear

--- tests/compound_bitwise_assignment.go
+++ tests/compound_bitwise_assignment.go
@@ -1,6 +1,17 @@
 package main
 
+func compoundBitwiseDynamic(value int) int {
+	return value
+}
+
 func appMain(args []string) int {
+	left := compoundBitwiseDynamic(12)
+	mask := compoundBitwiseDynamic(10)
+	if left&^mask != 4 {
+		print("FAIL dynamic &^\n")
+		return 1
+	}
+
 	value := 16
 	value >>= 2
 	if value != 4 {
