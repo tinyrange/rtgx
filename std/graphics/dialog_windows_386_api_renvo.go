67e2942325fdceab458bdadc079050aeba863a45
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-07-19 13:52:01 +1000

    Add native dialogs and IDE menu bar

--- std/graphics/dialog_windows_386_api_renvo.go
+++ std/graphics/dialog_windows_386_api_renvo.go
@@ -1,18 +1,18 @@
-//go:build rtg && windows && 386
+//go:build renvo && windows && 386
 
 package graphics
 
-// rtg:linkstatic comdlg32.dll,GetOpenFileNameA
+// renvo:linkstatic comdlg32.dll,GetOpenFileNameA
 func windowsGetOpenFileName(data *byte) int { return 0 }
 
-// rtg:linkstatic comdlg32.dll,GetSaveFileNameA
+// renvo:linkstatic comdlg32.dll,GetSaveFileNameA
 func windowsGetSaveFileName(data *byte) int { return 0 }
 
-// rtg:linkstatic shell32.dll,SHBrowseForFolderA
+// renvo:linkstatic shell32.dll,SHBrowseForFolderA
 func windowsBrowseForFolder(data *byte) int { return 0 }
 
-// rtg:linkstatic shell32.dll,SHGetPathFromIDListA
+// renvo:linkstatic shell32.dll,SHGetPathFromIDListA
 func windowsGetPathFromIDList(item, path int) int { return 0 }
 
-// rtg:linkstatic ole32.dll,CoTaskMemFree
+// renvo:linkstatic ole32.dll,CoTaskMemFree
 func windowsCoTaskMemFree(memory int) {}
